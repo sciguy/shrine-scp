7b965f93a5fd6c09b926a88f52063fe65459fc21
Jason Grant <jgrantk@gmail.com>
2021-10-14 10:54:34 -0600

    Make directory before transfer

--- lib/shrine/storage/scp.rb
+++ lib/shrine/storage/scp.rb
@@ -91,6 +91,7 @@ class Shrine
         def scp_up(id, tmp_path)
           FileUtils.chmod(permissions, tmp_path)
           destination = path(id)
+          bash "mkdir -p #{File.dirname(file_path)}"
           destination = "#{ssh_host}:#{destination}" if ssh_host
           scp_transfer(source: tmp_path, destination: destination)
         end
